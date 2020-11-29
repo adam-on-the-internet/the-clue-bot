39aa77187af7240de34cd95bc0afa1b3d3be00d6
Adam Callanan <a.callanan@sbcglobal.net>
2020-11-29 17:30:06 -0600

    working clue tracker

--- src/components/MysteryClueTracker.js
+++ src/components/MysteryClueTracker.js
@@ -5,13 +5,86 @@ class MysteryClueTracker extends Component {
     render() {
         const {mystery} = this.props;
 
+        let culpritClueTracker = "";
+        if (mystery.culpritOptions !== undefined) {
+            culpritClueTracker = buildClueTrackerTable("Suspect", mystery.culpritOptions, mystery.announcements);
+        }
+        let weaponClueTracker = "";
+        if (mystery.weaponOptions !== undefined) {
+            weaponClueTracker = buildClueTrackerTable("Weapon", mystery.weaponOptions, mystery.announcements);
+        }
+        let sceneClueTracker = "";
+        if (mystery.sceneOptions !== undefined) {
+            sceneClueTracker = buildClueTrackerTable("Scene", mystery.sceneOptions, mystery.announcements);
+        }
+
         return (
             <div>
-                <p>[Clue Tracker]</p>
+                <hr/>
+                <h3>Clue Trackers</h3>
+                <hr/>
+                <div style={trackerStyle}>
+                    {culpritClueTracker}
+                    {weaponClueTracker}
+                    {sceneClueTracker}
+                </div>
             </div>
         );
     }
 }
+
+function buildClueTrackerTable(tableName, choices, announcements) {
+    const choiceRows = choices.map((choice, index) => {
+        return buildClueTrackerRow(choice, announcements, index);
+    });
+    return (
+        <>
+            <h3>{tableName} Clue Tracker</h3>
+            <table>
+                <thead>
+                <tr>
+                    <th>{tableName}</th>
+                    <th>Eliminated</th>
+                    <th>Clue</th>
+                </tr>
+                </thead>
+                <tbody>
+                {choiceRows}
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+function buildClueTrackerRow(item, announcements, index) {
+    const relevantClue = announcements.find((clue) => {
+        return clue.includes("Clue #") && clue.includes(item);
+    });
+    const clueRevealed = relevantClue !== undefined;
+    let itemSymbol = "";
+    let itemClue = "";
+    if (clueRevealed) {
+        itemSymbol = "X";
+        itemClue = removeStatusFromAnnouncement(relevantClue, item);
+    }
+    return (
+        <tr key={index}>
+            <td>{item}</td>
+            <td>{itemSymbol}</td>
+            <td>{itemClue}</td>
+        </tr>
+    );
+}
+
+function removeStatusFromAnnouncement(announcement) {
+    const statusStart = announcement.indexOf("(The");
+    return announcement.slice(0, statusStart);
+}
+
+const trackerStyle = {
+    marginLeft: 20,
+    marginRight: 20,
+};
 
 MysteryClueTracker.propTypes = {
     mystery: PropTypes.object,
